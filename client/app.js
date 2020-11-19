3cc3e0df2b057a07f08b65a2e891477d4fb2d0ea
Tiffany Ma <yunshin.ma@gmail.com>
2020-11-19 15:24:54 -0500

    cleanup: removed spec files and cleaned up app.js

--- client/app.js
+++ client/app.js
@@ -1,27 +1,21 @@
 import React from 'react'
 
 import {Navbar, Search, SearchHook, SingleIngredient} from './components'
-import Routes from './routes'
 import {Route, Switch, BrowserRouter} from 'react-router-dom'
 
+// If our routes grow, we will import them from a separate routes file
+// import Routes from './routes'
+
 const App = () => {
-  const style = {color: 'red'}
   return (
     <div>
       <Navbar />
 
-      <h3 style={style}>Component built with Classes:</h3>
-      <Search />
-
-      <br />
-      <br />
-
-      <h3 style={style}>Component built with Hooks:</h3>
       <Switch>
         <Route path="/id/:id" component={SingleIngredient} />
         <Route component={SearchHook} />
       </Switch>
-      {/* <SearchHook /> */}
+
       {/* <Routes /> */}
     </div>
   )
